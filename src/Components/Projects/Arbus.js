dca4a6cd5b6800309424f2786bf15ffbd5453821
“helloandrewpaul” <mcgowanandrew@gmail.com>
2021-07-14 23:29:14 -0400

    arbus update

--- src/Components/Projects/Arbus.js
+++ src/Components/Projects/Arbus.js
@@ -6,24 +6,49 @@ console.log("data", data);
 const Arbus = () => {
   return (
     <BigWrap>
-      {data.map((arbus) => {
-        return (
-          
-            <Img src={arbus.img} />
-          
-        );
-      })}
+      <Wrap>
+        {data.slice(0,1).map((arbus) => {
+          return <Img src={arbus.img} />;
+        })}
+         {data.slice(1,14).map((arbus) => {
+          return <Img src={arbus.img} />;
+        })}
+      </Wrap>
     </BigWrap>
   );
 };
-
+const Wrap = styled.div`
+  margin-top: 90px;
+  padding-bottom: 40px;
+  width: 630px;
+  display: flex;
+  flex-wrap: wrap;
+  /* align-content: flex-start; */
+`;
 const Img = styled.img`
-  width: 600px;
-  height: auto;
+  margin: 0 15px 15px 0;
+
+  &:nth-child(odd) {
+    width: 75px;
+    height: 75px;
+    overflow: hidden;
+    object-fit: cover;
+  }
+  &:nth-child(even) {
+    width: 75px;
+    height: 75px;
+    overflow: hidden;
+    object-fit: cover;
+
+  }
+  &:nth-child(1) {
+    width: 615px;
+    height: auto;
+  }
 `;
 const BigWrap = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   margin-left: -30vw;
   padding-left: 30vw;
   background-color: rgba(0, 0, 0, 0.6);
