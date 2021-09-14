ee446d8a3640947294cae429421e8bf5fbac66cd
“Andrew <andrewpaulstudio@gmail.com>
2021-09-13 22:23:45 -0400

    small fixes

--- server/index.js
+++ server/index.js
@@ -1,42 +1,19 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const path = require('path');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-const PORT = 4000;
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.send('Working!')
+    
+})
 
 const { getAllProjects, getProjectByTitle } = require('./handlers');
-
-const app = express();
-
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, HEAD,GET, PUT, POST,DELETE'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-Width, Content-Tyope, Accept'
-  );
-  next();
-});
-
-app.use(morgan('tiny'));
-app.use(express.static('./server/assets'));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
-app.use('/', express.static(__dirname + '/'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-// app.get('/', (req, res) => { res.send('Hello from Express!')
-
 app.get('/projects', getAllProjects);
 app.get('/project/:title', getProjectByTitle);
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(process.env.PORT || PORT);
+app.listen(process.env.PORT || 5000)
+
