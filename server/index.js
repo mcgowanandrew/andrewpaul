c87557b036833cc5c12340ed2f2485beedf6ca1e
“Andrew <andrewpaulstudio@gmail.com>
2021-09-16 21:57:07 -0400

    server update

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static("public"))
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Working!')
-    
-})
+  res.send('Working!');
+});
 
 const { getAllProjects, getProjectByTitle } = require('./handlers');
 app.get('/projects', getAllProjects);
 app.get('/project/:title', getProjectByTitle);
 
-app.listen(process.env.PORT || 5000)
-
+app.listen(process.env.PORT || 5000);
