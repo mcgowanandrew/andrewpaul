5b887e9c7b84264195caf4d16037f288b3b24d4c
Andrew Paul <80865709+helloandrewpaul@users.noreply.github.com>
2021-09-25 05:57:54 -0400

    Update Project.js

--- client/src/Components/Projects/Project.js
+++ client/src/Components/Projects/Project.js
@@ -92,9 +92,8 @@ const Demo = styled.a`
 `;
 
 const DetailWrap = styled.div`
-  width: 600px;
-  margin: 15px 0;
-  padding: 15px;
+  width: 100%;
+  margin: 30px 0;
   display: flex;
   flex-direction: column;
 `;
