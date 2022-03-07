218eb05d54ec437a1fdcf202f16f97e1793d4be4
“Andrew <andrewpaulstudio@gmail.com>
2022-03-07 17:16:03 -0500

    refactored all projects and single project page

--- client/src/Components/Projects/Project.js
+++ client/src/Components/Projects/Project.js
@@ -8,7 +8,7 @@ const Project = () => {
   const [project, setProject] = useState([]);
 
   useEffect(() => {
-    fetch(`https://andrew-paul.herokuapp.com/project/${title}`, {
+    fetch(`/project/${title}`, {
       method: 'GET',
     })
       .then((res) => res.json())
