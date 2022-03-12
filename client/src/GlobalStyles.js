f9ff8325d70d2e435bcf001ff0c09fa036e20a55
“Andrew <andrewpaulstudio@gmail.com>
2022-03-11 19:42:09 -0500

    removed trailing white spaces

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,28 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
+
 body{
-    margin:0;
-    padding:0;
- 
+    margin: 0;
+    padding: 0;
     background-color: #030205;
-    color:#faf7ff;
+    color: #faf7ff;
     font-family: 'DM Sans', sans-serif;
 }
+
 a{
-    text-decoration:none;
-    color:#faf7ff;
+    text-decoration: none;
+    color: #faf7ff;
     margin-bottom: 3px;
     position: relative;
 }
+
 a:hover{
     color:#666;
     transition: all 0.3s ease-in-out;
 
 }
-
-    
-    
-    
-    `;
+`;
 
 export default GlobalStyle;
